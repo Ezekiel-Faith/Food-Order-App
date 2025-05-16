83541d99a596bab48ec8799b6775c264d13fc53d
Ezekie-Faith <ezekielfaith2000@gmail.com>
2025-05-16 05:02:34 +0100

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import foodLogo from '../assets/logo.jpg';
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header id='main-header'>
+      <div id='title'>
+        <img src={foodLogo} alt='Food Logo' />
+        <h1>Food-Order</h1>
+      </div>
+      <nav>
+        <button>Cart (0)</button>
+      </nav>
+    </header>
+  );
 }
