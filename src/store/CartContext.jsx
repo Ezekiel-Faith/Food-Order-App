3ba9e87d10546c3dd793276412f230b7adad1bd3
Ezekiel-faith <ezekielfaith20002gmail.com>
2025-05-21 13:41:31 +0100

    worked on the cart

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -30,12 +30,12 @@ function cartReducer(state, action) {
 
   if (action.type === 'REMOVE_ITEM') {
     const existingCartItemIndex = state.items.findIndex(
-      (item) => item.id === action.item.id
+      (item) => item.id === action.id
     );
     const existingCartItem = state.items[existingCartItemIndex];
+    const updatedItems = [...state.items];
 
     if (existingCartItem.quantity === 1) {
-      const updatedItems = [...state.items];
       updatedItems.splice(existingCartItemIndex, 1);
     } else {
       const updatedItem = {
@@ -59,7 +59,7 @@ export function CartContextProvider({ children }) {
   }
 
   function removeItem(id) {
-    dispatchCartAction({ type: 'REMOVE_ITEM' });
+    dispatchCartAction({ type: 'REMOVE_ITEM', id });
   }
 
   const cartContext = {
