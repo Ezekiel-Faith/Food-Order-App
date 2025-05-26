6a1f5b084d86892c6ab82fb06654a5da2a3051cb
Ezekiel-faith <ezekielfaith20002gmail.com>
2025-05-26 11:52:21 +0100

    added a custom http hook and also avoided common errors

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,23 +1,36 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
+import useHttp from '../hooks/useHttp';
 import MealItem from './MealItem';
 
+const requestConfig = {};
+
 export default function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
+  // const [loadedMeals, setLoadedMeals] = useState([]);
 
-  useEffect(() => {
-    async function fetchMeals() {
-      const response = await fetch('http://localhost:3000/meals');
+  // useEffect(() => {
+  //   async function fetchMeals() {
+  //     const response = await fetch('http://localhost:3000/meals');
 
-      if (!response) {
-        // ...
-      }
+  //     if (!response) {
+  //       // ...
+  //     }
 
-      const meals = await response.json();
-      setLoadedMeals(meals);
-    }
+  //     const meals = await response.json();
+  //     setLoadedMeals(meals);
+  //   }
 
-    fetchMeals();
-  }, []);
+  //   fetchMeals();
+  // }, []);
+
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp('http://localhost:3000/meals', requestConfig, []);
+
+  if (isLoading) {
+    return <p>Fetching meals...</p>;
+  }
 
   return (
     <ul id='meals'>
